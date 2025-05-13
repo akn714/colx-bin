9804542cfc32c79af270a30d0ab891e32973108e
akn714 <adarshkumar20012704@gmail.com>
2025-05-13 11:19:38 +0530

    removed some input fields from sell form

--- frontend/src/components/olx/Sell/Sell.jsx
+++ frontend/src/components/olx/Sell/Sell.jsx
@@ -4,19 +4,19 @@ export function Sell(){
     return (
         <>
             <form action="http://localhost:3001/api/olx/post_product" method='POST' className='post-form'>
-                <input name='name' type="text" className='post-name input' placeholder='your name' />
-                <div className='contact-div input'>
+                {/* <input name='name' type="text" className='post-name input' placeholder='your name' /> */}
+                {/* <div className='contact-div input'>
                     <div>+91</div>
                     <input name='contact' type="number" min={1000000000} max={9999999999} className='post-contact' placeholder='contect no.' />
-                </div>
-                <div className="price input">
-                    <div>₹</div>
-                    <input name='price' type="number" placeholder='00' />
-                </div>
+                </div> */}
                 <input name='title' type="text" className='post-title input' placeholder='What is the name of your product?' />
                 <div className='post-textarea'>
                     <p className="desc-title">Product description :</p>
                     <textarea name="desc" className='post-desc' cols="30" rows="10" placeholder='Let people know about your product...'></textarea>
+                </div>
+                <div className="price input">
+                    <div>₹</div>
+                    <input name='price' type="number" placeholder='00' />
                 </div>
                 <button type='submit' className='post-submit-btn'>submit</button>
             </form>
